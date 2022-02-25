e93cca7deac7eab506d791364d07897d5fb4f6ce
Ripon-Boss11 <100074010+Ripon-Boss11@users.noreply.github.com>
2022-02-25 17:38:01 +0600

    Ripon Cloner

---
+++ Ripon.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import random
+import time
+
+os.system('clear')
+os.system('pkg update')
+os.system('pkg upgrade')
+os.system('pkg install cmatrix')
+os.system('pkg install python -y')
+os.system('pkg install go -y')
+os.system('pkg install libcaca')
+os.system('pkg install mpv -y')
+os.system('pkg install python2')
+os.system('pkg install proot')
+os.system('clear')
+
+a="wait a mminutes "
+print(a)
+
+
+
+
+print("RIPON CLONER..................")
+
+
+os.system('clear')
+
+
+print("\t  U#####$###")
+
+
+os.system('clear')
+
+print("\t 64 bit can't Sapport ")
